3185ae900e9dee4d7bfc259f5071ace5c85600a6
Alexcmyk <alex.kirts@gmail.com>
2020-05-27 08:45:13 -0600

    add hamburger functionality and html

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const btnHambuger = document.querySelector('#btnHamburger');
+
+btnHambuger.addEventListener('click', function () {
+    console.log('ClickHamburger');
+
+    if (btnHambuger.classList.contains('open')) {
+        btnHambuger.classList.remove('open');
+    } else {
+        btnHambuger.classList.add('open');
+    }
+})
